e1bd324111fd5828e44dc6451a3f84e8010c8ed7
Agni17149 <agniroybar@gmail.com>
2024-09-28 13:05:31 +0530

    virtual env and import

---
+++ 33import.py
@@ -0,0 +1,17 @@
+# import math
+# a=math.floor(4.5789)
+# b=math.sqrt(9)
+# print(a,b)
+
+# from math import * #not recommended approach
+
+# from math import sqrt,pi as p
+# result= sqrt(9)*p
+# print(result)
+
+import math
+# from _33module import welcome, agni
+import _33module as m
+print(math.nan, type(math.nan))
+m.welcome()
+print(m.agni)
---
+++ _33module.py
@@ -0,0 +1,6 @@
+def welcome():
+    print("you are welcome")
+agni = 225
+# print(__name__) #main
+if __name__ =="__main__":
+    welcome()
