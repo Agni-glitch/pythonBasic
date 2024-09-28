7bf83fcdec2aea4867c4d1346d9f1fd5a256f5bd
Agni17149 <agniroybar@gmail.com>
2024-09-28 13:50:10 +0530

    os

---
+++ 34osmodule.py
@@ -0,0 +1,22 @@
+import os
+
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
+# for i in range(0,10):
+#     os.mkdir(f"data/Day{i+1}")
+
+# for i in range(0,10):
+#     os.rename(f"data/Day{i+1}",f"data/Tutorial{i+1}")
+
+# folders= os.listdir("data")
+# print(folders)
+
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"data/{folder}"))
+
+# print(os.getcwd())
+# os.chdir(r"C:\Users\AGNI\programming\python\pythonBasic")
+# os.chdir("C:/Users/AGNI/programming/python/pythonBasic")
+# os.chdir("C:\\Users\\AGNI\\programming\\python\\pythonBasic")
+# print(os.getcwd())
