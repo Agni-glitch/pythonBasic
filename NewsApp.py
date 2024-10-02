f159c29bbade467d01bc795cc75adecc06ef5cae
Agni17149 <agniroybar@gmail.com>
2024-10-02 09:03:13 +0530

    few fixs

--- NewsApp.py
+++ NewsApp.py
@@ -1,4 +1,4 @@
 import requests
-r = requests.get('https://newsapi.org/v2/everything?q=tesla&from=2024-09-01&sortBy=publishedAt&apiKey=0175f2ba50344b36b523c7b00ccf4101')
+r = requests.get('https://newsapi.org/v2/everything?q=tesla&from=2024-09-01&sortBy=publishedAt&apiKey=API_KEY')
 for i in range(100):
     print(f"{i+1}.  {r.json()['articles'][i]['content']}\n")
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-nt)
-# print(doc("div.c
